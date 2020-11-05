4f396f19e4a309870741d20f64ae34dc2be64f81
Your Name <your@email.com>
2020-11-05 23:17:48 +0000

    json-search

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require("request");
+const fs = require("fs");
+
+const searchBreed = process.argv[2];
+const url = "https://api.thecatapi.com/v1/breeds/search?q=" + searchBreed;
+const path = "./bread.json";
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log("There is an error occurred: ", error);
+    return;
+  }
+  const data = JSON.parse(body);
+  if (response.statusCode === 200) {
+    if (!data[0]) {
+      console.log("The breed you typed: " + searchBreed + " is not found!");
+    } else {
+      console.log(data[0].description);
+    }
+  }
+});
